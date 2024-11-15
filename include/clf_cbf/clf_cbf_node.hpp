da0297009bb82e69cf199fc1b41f7d90d34837ed
lfrecalde1 <lfrecalde1@espe.edu.ec>
2024-11-15 02:00:38 +0000

    CLF_CBF_CPP

--- include/clf_cbf/clf_cbf_node.hpp
+++ include/clf_cbf/clf_cbf_node.hpp
@@ -6,8 +6,8 @@
 #include <geometry_msgs/msg/vector3_stamped.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <eigen3/Eigen/Geometry>
-#include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/msg/point_stamped.hpp>
+#include <quadrotor_msgs/msg/position_command.hpp>
 
 namespace clf_cbf_node
 {
@@ -19,9 +19,15 @@ public:
 private:
     void quadrotorOdomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
     void payloadOdomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void payloadPointCallback(const geometry_msgs::msg::PointStamped::SharedPtr msg);
+    void positionCmdCallback(const quadrotor_msgs::msg::PositionCommand::SharedPtr msg);
 
     // Helper functions
     void publishCamera();
+    void updateStateFromQuadrotorOdometry();  
+    void updateStateFromPayloadOdometry();  
+    void updateStateFromPayloadCameraPoint();  
+    void updatePositionCmd();  
 
     // Timer
     rclcpp::TimerBase::SharedPtr timer_;
@@ -38,15 +44,22 @@ private:
     // Subscribers
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr quadrotor_odom_subscriber_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr payload_odom_subscriber_;
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr payload_camera_subscriber_;
+    rclcpp::Subscription<quadrotor_msgs::msg::PositionCommand>::SharedPtr position_cmd_subscriber_;
 
     // Data
     nav_msgs::msg::Odometry quadrotor_odometry_;  
     nav_msgs::msg::Odometry payload_odometry_;
+    geometry_msgs::msg::PointStamped payload_camera_point_;
+    quadrotor_msgs::msg::PositionCommand position_cmd_;
 
     // Vectors of my system
     Eigen::MatrixXf x_;
+    Eigen::MatrixXf p_;
     Eigen::MatrixXf result_;
+    Eigen::MatrixXf lyapunov_value_;
     float* result_ptr_;
+    float* lyapunov_ptr_;
 
 };
 }  // namespace clf_cbf_node
--- src/clf_cbf_node.cpp
+++ src/clf_cbf_node.cpp
@@ -3,6 +3,7 @@
 #include "clf_cbf/cbf.h"
 #include "clf_cbf/clf.h"
 
+
 namespace clf_cbf_node
 {
 
@@ -21,6 +22,24 @@ int call_cbf(float& cbf_result, const Eigen::MatrixXf& x) {
     int status = h_f(arg, res, iw, w, 0);
 
     cbf_result = output_result;
+    return status;
+}
+
+int call_clf(float& clf_result, const Eigen::MatrixXf& x, const Eigen::MatrixXf& p) {
+    const casadi_real* arg[2];
+    casadi_real* res[1];
+    casadi_real w[lyapunov_f_SZ_W] = {0};
+    casadi_int iw[lyapunov_f_SZ_IW] = {};
+
+    arg[0] = x.data();
+    arg[1] = p.data();
+    
+    casadi_real output_result = 0.0;
+    res[0] = &output_result;
+
+    int status = lyapunov_f(arg, res, iw, w, 0);
+
+    clf_result = output_result;
     return status;
 }
 
@@ -46,9 +65,13 @@ ClfCbfNode::ClfCbfNode(const rclcpp::NodeOptions &options)
 
     // Init Variables
     x_ = Eigen::MatrixXf::Zero(22, 1);
-    x_(0, 0) = 2.0;
+    p_ = Eigen::MatrixXf::Zero(27, 1);
+    p_(26, 0) = 1.0;
     result_ = Eigen::MatrixXf::Zero(1, 1);
+    lyapunov_value_ = Eigen::MatrixXf::Zero(1, 1);
+
     result_ptr_ = result_.data();
+    lyapunov_ptr_ = lyapunov_value_.data();
 
     // Subscriber to /quadrotor/odom
     quadrotor_odom_subscriber_ = this->create_subscription<nav_msgs::msg::Odometry>(
@@ -59,6 +82,14 @@ ClfCbfNode::ClfCbfNode(const rclcpp::NodeOptions &options)
     payload_odom_subscriber_ = this->create_subscription<nav_msgs::msg::Odometry>(
         "/" + quad_name_ + "/payload/odom", 10, 
         std::bind(&ClfCbfNode::payloadOdomCallback, this, std::placeholders::_1));
+
+    payload_camera_subscriber_ = this->create_subscription<geometry_msgs::msg::PointStamped>(
+        "/" + quad_name_ + "/payload/point", 10, 
+        std::bind(&ClfCbfNode::payloadPointCallback, this, std::placeholders::_1));
+
+    position_cmd_subscriber_ = this->create_subscription<quadrotor_msgs::msg::PositionCommand>(
+        "/" + quad_name_ + "/position_cmd", 10, 
+        std::bind(&ClfCbfNode::positionCmdCallback, this, std::placeholders::_1));
     
     // Publisher for vector3_stamped
     clf_publisher_ = this->create_publisher<geometry_msgs::msg::PointStamped>("/" + quad_name_ + "/" + "payload" + "/clf", 10);
@@ -73,30 +104,151 @@ ClfCbfNode::ClfCbfNode(const rclcpp::NodeOptions &options)
 void ClfCbfNode::quadrotorOdomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
     quadrotor_odometry_ = *msg;
+    updateStateFromQuadrotorOdometry();
 }
 
 void ClfCbfNode::payloadOdomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
     payload_odometry_ = *msg;
+    updateStateFromPayloadOdometry();
+}
+
+void ClfCbfNode::payloadPointCallback(const geometry_msgs::msg::PointStamped::SharedPtr msg)
+{
+    payload_camera_point_ = *msg;
+    updateStateFromPayloadCameraPoint();
+}
+
+void ClfCbfNode::positionCmdCallback(const quadrotor_msgs::msg::PositionCommand::SharedPtr msg)
+{
+    position_cmd_ = *msg;
+    updatePositionCmd();  
 }
 
 void ClfCbfNode::publishCamera()
 {
-    float aux = 25.3;
-    int status_1;
-    x_ = Eigen::MatrixXf::Random(22, 1);
+    int status_cbf;
+    int status_clf;
+    // Using CBF and CLF Casadi
+    status_cbf = call_cbf(*(result_ptr_), x_);
+    status_clf = call_clf(*(lyapunov_ptr_), x_, p_);
 
-    // Using CBF Casadi
-    status_1 = call_cbf(*(result_ptr_), x_);
-    // Publish vector as a point 
+    // Publish vector as a point for cbf
+    geometry_msgs::msg::PointStamped cbf_msg;
+    cbf_msg.header.frame_id = camera_name_;
+    cbf_msg.header.stamp = this->get_clock()->now();
+    cbf_msg.point.x = 0.0;
+    cbf_msg.point.y = result_(0, 0);
+    cbf_msg.point.z = 0.0;
+    cbf_publisher_->publish(cbf_msg);
+
+    // Publish vector as a point for clf
     geometry_msgs::msg::PointStamped clf_msg;
     clf_msg.header.frame_id = camera_name_;
     clf_msg.header.stamp = this->get_clock()->now();
-    clf_msg.point.x = aux;
-    clf_msg.point.y = x_(0, 0);
-    clf_msg.point.z = result_(0, 0);
+    clf_msg.point.x = 0.0;
+    clf_msg.point.y = lyapunov_value_(0, 0);
+    clf_msg.point.z = 0.0;
     clf_publisher_->publish(clf_msg);
 
+}
+
+void ClfCbfNode::updateStateFromQuadrotorOdometry()
+{
+    // Ensure x_ is the correct size (22x1 matrix as specified)
+    if (x_.rows() != 22 || x_.cols() != 1) {
+        RCLCPP_ERROR(this->get_logger(), "Matrix x_ has incorrect dimensions. Expected 22x1.");
+        return;
+    }
+    x_(6, 0) = quadrotor_odometry_.pose.pose.position.x;
+    x_(7, 0) = quadrotor_odometry_.pose.pose.position.y;
+    x_(8, 0) = quadrotor_odometry_.pose.pose.position.z;
+
+    x_(9, 0) = quadrotor_odometry_.twist.twist.linear.x;
+    x_(10, 0) = quadrotor_odometry_.twist.twist.linear.y;
+    x_(11, 0) = quadrotor_odometry_.twist.twist.linear.z;
+
+    x_(12, 0) = quadrotor_odometry_.pose.pose.orientation.w;
+    x_(13, 0) = quadrotor_odometry_.pose.pose.orientation.x;
+    x_(14, 0) = quadrotor_odometry_.pose.pose.orientation.y;
+    x_(15, 0) = quadrotor_odometry_.pose.pose.orientation.z;
+
+    x_(16, 0) = quadrotor_odometry_.twist.twist.angular.x;
+    x_(17, 0) = quadrotor_odometry_.twist.twist.angular.y;
+    x_(18, 0) = quadrotor_odometry_.twist.twist.angular.z;
+}
+
+void ClfCbfNode::updateStateFromPayloadOdometry()
+{
+    // Ensure x_ is the correct size (22x1 matrix as specified)
+    if (x_.rows() != 22 || x_.cols() != 1) {
+        RCLCPP_ERROR(this->get_logger(), "Matrix x_ has incorrect dimensions. Expected 22x1.");
+        return;
+    }
+    x_(0, 0) = quadrotor_odometry_.pose.pose.position.x;
+    x_(1, 0) = quadrotor_odometry_.pose.pose.position.y;
+    x_(2, 0) = quadrotor_odometry_.pose.pose.position.z;
+
+    x_(3, 0) = quadrotor_odometry_.twist.twist.linear.x;
+    x_(4, 0) = quadrotor_odometry_.twist.twist.linear.y;
+    x_(5, 0) = quadrotor_odometry_.twist.twist.linear.z;
+}
+
+void ClfCbfNode::updateStateFromPayloadCameraPoint()
+{
+    // Ensure x_ is the correct size (22x1 matrix as specified)
+    if (x_.rows() != 22 || x_.cols() != 1) {
+        RCLCPP_ERROR(this->get_logger(), "Matrix x_ has incorrect dimensions. Expected 22x1.");
+        return;
+    }
+    
+    x_(19, 0) = payload_camera_point_.point.x;
+    x_(20, 0) = payload_camera_point_.point.y;
+    x_(21, 0) = payload_camera_point_.point.z;
+}
+
+void ClfCbfNode::updatePositionCmd()
+{
+    // Ensure x_ is the correct size (22x1 matrix as specified)
+    if (p_.rows() != 27 || p_.cols() != 1) {
+        RCLCPP_ERROR(this->get_logger(), "Matrix p_ has incorrect dimensions. Expected 22x1.");
+        return;
+    }
+    p_(0, 0)= position_cmd_.points[0].position.x;
+    p_(1, 0)= position_cmd_.points[0].position.y;
+    p_(2, 0)= position_cmd_.points[0].position.z;
+
+    p_(3, 0)= position_cmd_.points[0].velocity.x;
+    p_(4, 0)= position_cmd_.points[0].velocity.y;
+    p_(5, 0)= position_cmd_.points[0].velocity.z;
+
+    p_(6, 0)= position_cmd_.points[0].position_quad.x;
+    p_(7, 0)= position_cmd_.points[0].position_quad.y;
+    p_(8, 0)= position_cmd_.points[0].position_quad.z;
+
+    p_(9, 0)=  position_cmd_.points[0].velocity_quad.x;
+    p_(10, 0) = position_cmd_.points[0].velocity_quad.y;
+    p_(11, 0) = position_cmd_.points[0].velocity_quad.z;
+
+    p_(12, 0) = position_cmd_.points[0].quaternion.w;
+    p_(13, 0) = position_cmd_.points[0].quaternion.x;
+    p_(14, 0) = position_cmd_.points[0].quaternion.y;
+    p_(15, 0) = position_cmd_.points[0].quaternion.z;
+
+    p_(16, 0) = position_cmd_.points[0].angular_velocity.x;
+    p_(17, 0) = position_cmd_.points[0].angular_velocity.y;
+    p_(18, 0) = position_cmd_.points[0].angular_velocity.z;
+
+    p_(19, 0) = 0.0;
+    p_(20, 0) = 0.0;
+    p_(21, 0) = 0.0;
+
+    p_(22, 0) = 0.0;
+    p_(23, 0) = 0.0;
+    p_(24, 0) = 0.0;
+    p_(25, 0) = 0.0;
+
+    p_(26, 0) = 1.0;
 }
 
 // Register the component
